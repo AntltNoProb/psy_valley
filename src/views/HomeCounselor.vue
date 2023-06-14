b6bcf08a628ce5e6bc876ca8c64be9469f5737ac
Antlt_J <gingerant@sina.com>
2023-06-14 14:16:01 +0800

    add: added schedule viewer to home page

--- src/views/HomeCounselor.vue
+++ src/views/HomeCounselor.vue
@@ -1,24 +1,41 @@
 <template>
-    <div style="padding: 10px">
+    <div>
         <!--    功能区域-->
-        <div style="margin: 10px 0">
-            <el-row>
-                <el-col><el-card></el-card></el-col>
-                <el-col><el-card></el-card></el-col>
+        <el-main style="margin: 10px 10px;">
+            <el-row :gutter="10">
+                <el-col :span=12><constructingPage /></el-col>
+                <el-col :span=12><scheduleViewer /></el-col>
             </el-row>
-            <el-row>
-                <el-col><el-card></el-card></el-col>
-                <el-col><el-card></el-card></el-col>
+            <el-row :gutter="10">
+                <el-col :span=16><constructingPage /></el-col>
+                <el-col :span=8><constructingPage /></el-col>
             </el-row>
-        </div>
+        </el-main>
 
     </div>
 </template>
 <script>
-
+import constructingPage from "@/components/homePage/constructingPage.vue";
+import scheduleViewer from "@/components/homePage/scheduleViewer.vue";
 export default {
     name: 'VueHome',
     components: {
-    }
+        constructingPage,
+        scheduleViewer,
+    },
 }
 </script>
+<style>
+.el-row {
+    margin-bottom: 20px;
+}
+
+.el-row:last-child {
+    margin-bottom: 0;
+}
+
+
+.grid-content {
+    min-height: 200px;
+}
+</style>
